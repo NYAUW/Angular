d974ba98209260f01c9cf06e03ee1dd594ac8a86
Lucas Ribeiro <lucas.ribeiro@contmatic.com.br>
2020-02-06 17:01:21 -0300

    7 horas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AsusComponent } from './asus/asus.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'asus', component: AsusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
