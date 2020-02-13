f8ff902316c1c54fc39d5204b9bc2669d021d798
Lucas Ribeiro <lucas.ribeiro@contmatic.com.br>
2020-02-13 16:56:08 -0300

    O site ta fofinho

--- src/app/google/google.component.ts
+++ src/app/google/google.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './google.component.html',
   styleUrls: ['./google.component.css']
 })
-export class GoogleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class GoogleComponent {
+  
+  public googlePixel : number = 1800;
+  public googlePixel4 : number = 2300;
+  public googlePixe3A : number = 1750;
+  public googlePixe4XL : number = 2350;
+  public googlePixeXL : number = 1650;
+  public googlePixe2XL : number = 1999;
+  public googlePixeC : number = 1350;
 }
